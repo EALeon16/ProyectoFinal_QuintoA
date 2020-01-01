0455b52fecdf00742628e8b0e3c9575b226a42a8
EALeon16 <eduardo.leon@unl.edu.ec>
2020-01-01 18:01:14 -0500

    agregar horario

--- apps/horario/views.py
+++ apps/horario/views.py
@@ -5,33 +5,32 @@ from apps.modelo.models import Horario, Pelicula, Sala
 
 
 def agregarHorario(request):
-    
-    """lista = Pelicula.objects.all()
-    
-listas = Sala.objects.all()
+    lista = Pelicula.objects.all()
+    listas= Sala.objects.all()
     formulario_horario = FormularioHorario(request.POST)
     if request.method == 'POST':
         if formulario_horario.is_valid():
-            datosH = formulario_horario.cleaned_data
+            datos = formulario_horario.cleaned_data
             horario = Horario()
-            horario.hora_pelicula = datosH.get('hora_pelicula')
-            horario.fecha_pelicua = datosH.get('fecha_pelicua')
-            horario.sala_id = datosH.get('sala')
-            horario.sala_id = datosH.get('sala')
+            horario.hora_pelicula = datos.get('hora_pelicula')
+            horario.fecha_pelicua = datos.get('fecha_pelicua')
+            horario.pelicula = datos.get('pelicula')
+            horario.sala = datos.get('sala')
             horario.save()
             messages.success(request, 'Horario registrado correctamente')
-            return redirect(verPelicula)
+            return redirect('/cartelera')
         else:
             messages.error(request, 'Esta pelicula ya esta registrada')
-               
-
-    fecha = []
+                 
+   
+    
     context = {
-        'lista' : lista,
-        'listas': listas,
+        'f': formulario_horario,
+        'lista': lista,
+        'listas': listas
     }
     
-    return render(request,'horario/agregar_horario.html')"""
+    return render(request,'horario/agregar_horario.html', context)
 
 
 
